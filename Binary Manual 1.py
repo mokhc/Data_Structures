89826398d7349d12060c94f70903cfb2609bd156
mokhc <mokhonchuen@gmail.com>
2025-11-11 14:37:38 +0800

    Add files via upload

---
+++ Binary Manual 1.py
@@ -0,0 +1,27 @@
+# Test - Binary Tree
+# Create a binary tree manually
+# @mokhc
+# 11/11/25
+from print_btree import print_btree
+
+# class BNode
+class BNode():
+    def __init__(self, v, l='', r=''):
+        self.val = v
+        self.left = l
+        self.right = r
+
+# implementation 1
+t1 = BNode(1)
+t1.left = BNode(2, BNode(4, BNode(8)), BNode(5))
+t1.right = BNode(3, BNode(6))
+print_btree(t1)
+"""
+   ___1___
+   |     |
+ __2__  _3
+ |   |  |
+_4   5  6
+|
+8
+"""
---
+++ Binary Manual 2.py
@@ -0,0 +1,31 @@
+# Test - Binary Tree
+# Create a binary tree manually
+# @mokhc
+# 11/11/25
+from print_btree import print_btree
+
+# class BNode
+class BNode():
+    def __init__(self, v, l="", r=""):
+        self.val = v
+        self.left = l
+        self.right = r
+
+# implementation 1
+t1 = BNode(1,)
+t1.left = BNode(2)
+t1.right = BNode(3)
+t1.left.left = BNode(4)
+t1.left.right = BNode(5)
+t1.right.left = BNode(6)
+t1.left.left.left = BNode(8)
+print_btree(t1)
+"""
+   ___1___
+   |     |
+ __2__  _3
+ |   |  |
+_4   5  6
+|
+8
+"""
