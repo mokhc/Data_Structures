2acb5dd48dd5bc11312c56463a5af63e42263fa2
mokhc <mokhonchuen@gmail.com>
2025-11-15 11:23:43 +0800

    Add files via upload

---
+++ Singly Linked List 1.py
@@ -0,0 +1,25 @@
+# Test - Singly Linked List
+# Create a singly linked list manually
+# @mokhc
+# 15/11/25
+
+# class SLLNode
+class SLLNode:
+    def __init__(s, input, next=''):
+        s.val = input
+        s.next = next
+    
+# set up singly linked list
+start1a = SLLNode(5)
+start1a.next = SLLNode(4)
+start1a.next.next = SLLNode(3)
+start1a.next.next.next = SLLNode(2)
+start1a.next.next.next.next = SLLNode(1)
+
+# loop through the singly linked list
+start2a = start1a
+while start2a is not None:
+    if start2a:
+        start2a = start2a.next
+    else:
+        break
---
+++ Singly Linked List 2.py
@@ -0,0 +1,21 @@
+# Test - Singly Linked List
+# Create a singly linked list manually
+# @mokhc
+# 15/11/25
+
+# class SLLNode
+class SLLNode:
+    def __init__(s, input, next=''):
+        s.val = input
+        s.next = next
+    
+# set up singly linked list
+start1b = SLLNode(5, SLLNode(4, SLLNode(3, SLLNode(2, SLLNode(1)))))
+
+# loop through the singly linked list
+start2b = start1b
+while start2b is not None:
+    if start2b:
+        start2b = start2b.next
+    else:
+        break
